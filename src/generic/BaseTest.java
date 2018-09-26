c146c955005412136d6f5e785a67739bdd81ed5b
shilpa <shilpamadabhavi@gmail.com>
2018-09-26 15:12:56 +0530

    update

--- src/generic/BaseTest.java
+++ src/generic/BaseTest.java
@@ -1,3 +1,10 @@
+
+//this code works for any project
+//in testng class make all the variable as public
+//dont use @Test in the base test 
+//if we make webdriver as staic parallel execution is not posiible
+//if webdriver is final we cannot change the browser
+//donot hard code the input always store in a variable and use
 package generic;
 
 import java.util.concurrent.TimeUnit;
@@ -6,15 +13,26 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-
-public class BaseTest {
-	WebDriver driver;
+import org.testng.annotations.Listeners;
+@Listeners(Result.class)
+abstract public class BaseTest implements IAutoConst {
+	
+	public WebDriver driver;
+	public String url=Utility.getPropertyValue(CONFIG_PATH, "URL");//dont make url as static bcz its keep changing
+	String ITO=Utility.getPropertyValue(CONFIG_PATH, "ITO");
+	public long duration=Long.parseLong(ITO);//dont make duration as static bcz its keep changing
+	static
+	{
+		System.setProperty(chrome_key,chrome_value);
+		System.setProperty(gecko_key, gecko_value);
+	}
+	
 	@BeforeMethod
 public void openApplication()
 {
 	driver=new ChromeDriver();
-	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-	driver.get("https://www.google.com/");
+	driver.manage().timeouts().implicitlyWait(duration, TimeUnit.SECONDS);
+	driver.get(url);
 	
 }
 	@AfterMethod
---
+++ src/generic/IAutoConst.java
@@ -0,0 +1,9 @@
+package generic;
+
+public interface IAutoConst {
+	 String chrome_key="webdriver.chrome.driver";
+	 String gecko_key="webdriver.gecko.driver";
+	 String chrome_value="./driver/chromedriver.exe";
+	 String gecko_value="./driver/geckodriver.exe";
+	 String CONFIG_PATH="./config.properties";
+}
